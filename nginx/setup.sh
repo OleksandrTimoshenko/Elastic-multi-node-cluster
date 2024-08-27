9de924556ff5978d2b4554418d1e9ce6a648393e
Oleksandr <o.tymoshenko@astoundcommerce.com>
2024-08-27 18:12:25 +0300

    add role for Nginx setup

--- nginx/setup.sh
+++
@@ -1,7 +0,0 @@
-# in host mashine all `192.168.56.12   kibana.com` to /etc/hosts/
-sudo apt install nginx -y
-cd /etc/nginx/sites-available/
-sudo nano kibana.conf
-sudo ln -s /etc/nginx/sites-available/kibana.conf /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo systemctl restart nginx
